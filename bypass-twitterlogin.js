d0a5d877d44d2bc1fdfa50bbc76aad9ef3e50a5a
Tech Help For Anyone <techhelpforanyone2@gmail.com>
2022-06-01 20:39:33 -0400

    Create bypass-twitterlogin.js

---
+++ bypass-twitterlogin.js
@@ -0,0 +1,5 @@
+javascript: 
+document.querySelectorAll('[role="dialog"]')[document.querySelectorAll('[role="dialog"]').length - 1].remove(); 
+document.getElementsByTagName("html")[0].style.overflowY = "scroll";
+void 0;
+//COPY ALL CODE AND PASTE INTO BOOKMARK! TO USE, CLICK THE BOOKMARK
